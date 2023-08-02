edbb00aa7098141829e4c77ddca76a9e113cee73
Patrick <patrick@pop-os.localdomain>
2023-08-01 21:50:00 -0700

    added game name and logic

--- lib/lv/lobby.ex
+++ lib/lv/lobby.ex
@@ -1,5 +1,5 @@
 defmodule Lv.Lobby do
-  defstruct [:player1_pid, :player2_pid, :game_server, :status, :id]
+  defstruct [:player1_pid, :player2_pid, :game_server, :status, :id, :game]
 
   alias Lv.GameServer
 
@@ -10,18 +10,24 @@ defmodule Lv.Lobby do
           player2_pid: pid,
           game_server: pid,
           status: status, 
-          id: integer | nil 
+          id: integer | nil,
+          game: String.t()
         }
 
 
   def new(id, module, player) when is_integer(id) do
     {:ok, server} = GameServer.start([module: module, player: player])
+    game = case module do
+      Lv.ConnectFour.Game -> "connectfour"
+      Lv.TicTacToe.Game -> "tictactoe"
+    end
     %__MODULE__{
       id: id,
       player1_pid: nil,
       player2_pid: nil,
       game_server: server,
-      status: :waiting_for_opponent
+      status: :waiting_for_opponent,
+      game: game
     }
   end
 end
