1bc04144d61ee9e0750f967d42271d1bb0bff9d7
Patrick <patrick@pop-os.localdomain>
2023-08-15 14:45:50 -0700

    call to new computer player with options

--- lib/lv/connect_four/game.ex
+++ lib/lv/connect_four/game.ex
@@ -76,8 +76,8 @@ defmodule Lv.ConnectFour.Game do
   def computer_turn(%__MODULE__{board: board, computer_difficulty: comp_difficulty} = game) do
     computer_move =
       case comp_difficulty do
-        :random -> ComputerPlayer.move(board, :random)
-        :perfect -> ComputerPlayer.move(game, :perfect)
+        :random -> Lv.ComputerPlayer.move(board, :random)
+        :perfect -> Lv.ComputerPlayer.move(game, :perfect, look_ahead: 6)
       end
 
     {:ok, board} = Board.mark(board, computer_move, :black)
