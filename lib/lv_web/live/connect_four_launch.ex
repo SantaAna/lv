cc50eec6e4462eceb202489d4d88f7857f080e05
Patrick Struthers <patrickstruthers@Patricks-MacBook-Air.local>
2023-08-15 23:09:29 -0700

    change home layout

--- lib/lv_web/live/connect_four_launch.ex
+++ lib/lv_web/live/connect_four_launch.ex
@@ -1,7 +1,7 @@
 defmodule LvWeb.ConnectFourLaunch do
   use LvWeb, :live_view
   alias Phoenix.PubSub
-  alias Lv.LobbyServer 
+  alias Lv.LobbyServer
 
   @alias_to_game_module %{
     "connectfour" => [Lv.ConnectFour.Game, LvWeb.ConnectFour, "connectfour"],
@@ -18,35 +18,32 @@ defmodule LvWeb.ConnectFourLaunch do
 
   def render(assigns) do
     ~H"""
-    <h1 class="text-4xl mb-3"> Start Playing </h1>
+    <h1 class="text-4xl mb-3">Start Playing</h1>
     <div class="flex flex-col gap-4">
-      <p> 
-      Join an existing lobby from the list below to play against a human opponenent.
-      If you don't see a game you are interested in create your own lobby and wait for an opponent.
+      <p>
+        Join an existing lobby from the list below to play against a human opponenent.
+        If you don't see a game you are interested in create your own lobby and wait for an opponent.
       </p>
-      <p> 
-      If your looking for a challenge start a game against a computer opponent.
-      The computer will always be planning a few moves ahead so make sure to think twice before you move.
+      <p>
+        If your looking for a challenge start a game against a computer opponent.
+        The computer will always be planning a few moves ahead so make sure to think twice before you move.
       </p>
     </div>
-    <.link navigate={~p"/match-activity"}>
-    <.link_button> Check the Match Feed </.link_button>
-    </.link>
     <div class="grid grid-cols-2 gap-4">
-        <.link navigate={~p"/connectfour"}> 
+      <.link navigate={~p"/connectfour"}>
         <.link_button>Play Connect Four Against Computer</.link_button>
-        </.link>
-        <.link_button phx-click="create-lobby" phx-value-game="connectfour">
-          Create Connect Four Lobby
-        </.link_button>
-        <.link navigate={~p"/ttt"}> 
+      </.link>
+      <.link_button phx-click="create-lobby" phx-value-game="connectfour">
+        Create Connect Four Lobby
+      </.link_button>
+      <.link navigate={~p"/ttt"}>
         <.link_button>Play TicTacToe Against Computer</.link_button>
-        </.link>
-        <.link_button phx-click="create-lobby" phx-value-game="tictactoe">
-          Create TicTacToe Lobby
-        </.link_button>
+      </.link>
+      <.link_button phx-click="create-lobby" phx-value-game="tictactoe">
+        Create TicTacToe Lobby
+      </.link_button>
     </div>
-    <h1 class="text-2xl mb-3 mt-6"> Lobbies </h1>
+    <h1 class="text-2xl mb-3 mt-6">Lobbies</h1>
     <div class="flex flex-col gap-5">
       <.lobby :for={lobby <- @lobbies} lobby={lobby} />
     </div>
@@ -60,7 +57,7 @@ defmodule LvWeb.ConnectFourLaunch do
         <%= @lobby.game %>
       </div>
       <.link_button phx-click="join" phx-value-id={@lobby.id} phx-value-game={@lobby.game}>
-      Join
+        Join
       </.link_button>
     </div>
     """
